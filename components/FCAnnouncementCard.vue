0766aa766c1c91eedee74748c53ccf021505a268
ZachLeeTech <zachary647@outlook.com>
2022-10-31 14:50:40 -0400

    completed home page and relevant components

---
+++ components/FCAnnouncementCard.vue
@@ -0,0 +1,51 @@
+<script lang="ts" setup>
+    const props = defineProps<{
+    title: string
+    text: string
+    }>()
+</script>
+
+<template>
+  <div class="announcementcard">
+    <h1>{{ props.title }}</h1>
+    <div class="text"> {{ text }}</div>
+  </div>
+</template>
+
+
+<style scoped>
+    h1{
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      margin-top: 10px;
+      margin-left: 13px;
+      margin-right: 13px;
+      text-align: left;
+      font-weight: 700;
+      font-size: 20px;
+    }
+
+    .text{
+      text-overflow: ellipsis;
+      overflow: hidden;
+      margin-top: 7px;
+      margin-left: 13px;
+      margin-right: 13px;
+      text-align: left;
+      font-size: 12px;
+      font-weight: 400;
+      line-height: 14px;
+      height: 42px;
+    }
+
+    .announcementcard{
+      color: #FFFFFF;
+      background: rgba(0, 89, 84, 0.84);
+      display: inline-block;
+      width: 328px;
+      height: 110px;
+      left: 16px;
+      border-radius: 10px;
+    }
+</style>
