f051def3f27d1bf415b13f326e21d1f9a51fede1
Zachary Lee <83090310+ZachLeeTech@users.noreply.github.com>
2022-10-31 22:50:53 -0400

    Completed home page and relevant components (#24)
    * completed home page and relevant components
    
    * added page meta at the top of the script

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,87 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  definePageMeta({
+    title: 'Frontier College',
+    showBack: false
+  })
+  
+  const name = 'Greg'
+  const title = 'New Position Openings'
+  const content = "Come one come all to the grand opening of FIFTEEN new " +
+  "position openings on the job board! These non-profits need your help to " +
+  "better the community, so get blah blah blah blah blah blah blah blah " +
+  "blah blahblah blahblah blahblah blah"
+</script>
 
 <template>
   <div>
-    <h1 w:text="green" w:bg="light-blue">Home Page</h1>
-    <FCButton text="Button" />
-    <Icon name="fe:check-circle-o" w:text="green" />
+    <div class="temporary-header">
+    </div>
+
+    <div class="welcome-card">
+      <h1>Welcome, {{ name }}!</h1>
+    </div>
+
+    <div class="immediate-announcements">
+      <FCAnnouncementCard :title=title :text="content"/>
+      <FCViewAll/>
+    </div>
+
+    <div class="arrow-cards">
+      <FCArrowCard title='My Assigned Programs' :text="content" color="#98BF1E"/>
+      <FCArrowCard title='Group Chat' :text="content" color="#FECE3A"/>
+      <FCArrowCard title='Community Portal' :text="content" color="#EA7123"/>
+    </div>
+
+    <div class="whitebar"></div>
   </div>
 </template>
+
+<style scoped>
+  * {
+    font-family: 'Inter';
+  }
+
+  .whitebar{
+    position: absolute;
+    width: 7px;
+    height: 416px;
+    left: 273px;
+    top: 339px;
+
+    background: #FFFFFF;
+  }
+
+ .temporary-header{
+  width: 360px;
+  height: 55px;
+  left: 0px;
+  top: 0px;
+  background: #005954;
+ }
+ .welcome-card{
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 29px;
+  padding: 13px;
+  width: 360px;
+  height: 59px;
+  left: 0px;
+  top: 56px;
+ }
+
+ .immediate-announcements{
+  padding-top: 25px;
+  text-align: center;
+  width: 360px;
+  height: 210px;
+  left: 0px;
+  top: 114px;
+
+  background: #E1E1E1;
+  box-shadow: inset 0px 4px 4px rgba(0, 0, 0, 0.25);
+ }
+
+ .arrow-cards{
+  position: relative;
+ }
+</style>
