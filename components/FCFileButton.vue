739ea5f0346a5e8c9cb30e24d8c388e7eb304898
shawnpl7 <90327099+shawnpl7@users.noreply.github.com>
2022-10-31 22:52:21 -0400

    Add program pages and components (#25)

---
+++ components/FCFileButton.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+const props = defineProps<{
+    text: string
+}>()
+</script>
+
+<template>
+    <button w:bg="tan">
+        <Icon name="fe:file" class="button-icon"/>
+        <div class="button-text">{{ props.text }}</div>
+    </button>
+</template>
+
+<style scoped>
+button {
+    font-size: 14px;
+    border-radius: 31px;
+    margin: 10px;
+    width: 126px;
+    height: 126px;
+    position: relative;
+}
+
+.button-text {
+    position: absolute;
+    bottom: 10%;
+    width: 100%;
+    left: 0%;
+}
+
+.button-icon {
+    position: absolute;
+    bottom: 30%;
+    width: 100%;
+    left: 0%;
+    font-size: 80px;
+}
+</style>
