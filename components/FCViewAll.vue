931f83d91ad65f2d46d733ca87660931703aa110
Zachary Lee <83090310+ZachLeeTech@users.noreply.github.com>
2022-10-31 22:50:53 -0400

    Completed home page and relevant components (#24)
    * completed home page and relevant components
    
    * added page meta at the top of the script

---
+++ components/FCViewAll.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div>
+    <button> View All </button>
+  </div>
+</template>
+
+
+<style scoped>
+    div{
+      text-align: right;  
+    }
+ button{
+    margin-top: 10px;
+    margin-right: 16px;
+    width: 127px;
+    height: 45px;
+
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+    border-radius: 10px;
+    color: #FFFFFF;
+    background: #005954;
+ }
+</style>
