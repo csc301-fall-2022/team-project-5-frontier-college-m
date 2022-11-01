88d5ae85f557c58fb7db48a0da236550f4c6b22f
Shawn Plotko <shawn.plotko@mail.utoronto.ca>
2022-10-31 22:29:05 -0400

    Add program pages and components

---
+++ components/FCTag.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+const props = defineProps<{
+    text: string
+    icon: Text
+}>()
+</script>
+
+<template>
+    <div class="tag">
+        <Icon :name=props.icon class="tag-icon"/>
+        <div class="tag-text">{{ props.text }}</div>
+    </div>
+</template>
+
+<style scoped>
+.tag {
+    width:fit-content;
+    border-radius: 31px;
+    display: flex;
+    padding: 5px;
+    margin: 10px;
+}
+
+.tag-icon {
+    font-size: 20;
+    margin-right: 5px;
+}
+
+.tag-text {
+    font-size: 11px;
+}
+</style>
