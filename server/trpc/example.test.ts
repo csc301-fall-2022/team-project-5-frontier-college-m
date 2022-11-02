f0b17504dc73e7f2988348a838355cbb9ecb9e51
Patrick Zhou <44577726+PatrickZhou17@users.noreply.github.com>
2022-11-02 14:43:53 -0400

    test for event endpoint

--- server/trpc/example.test.ts
+++ server/trpc/example.test.ts
@@ -1,5 +1,6 @@
 import { createContext } from './createContext'
 import { router } from './router'
+import { td2Data } from './d2-dummy-data'
 
 test('example tRPC test', async () => {
   const ctx = await createContext()
@@ -7,4 +8,20 @@ test('example tRPC test', async () => {
 
   const count = await caller.query('hello')
   expect(count).toBeGreaterThanOrEqual(0)
+})
+
+test('even tRPC test name', async () => {
+  const ctx = td2Data 
+  const caller = router.createCaller(ctx)
+
+  const answer = await caller.query('get_eventDetails')
+  expect(answer[0].name).toBe('Figma Tutoring') 
+})
+
+test('even tRPC test type', async () => {
+  const ctx = td2Data 
+  const caller = router.createCaller(ctx)
+
+  const answer = await caller.query('get_eventDetails')
+  expect(answer[1].type).toBe('recurring') 
 })
