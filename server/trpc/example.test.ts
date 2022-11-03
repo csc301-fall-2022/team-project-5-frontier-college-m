fa337ec9f9029adfaf25d4d8b295329ec333d0cd
Ricky Yi <ricky.yi@mail.utoronto.ca>
2022-11-03 00:39:35 -0400

    add user endpoint

--- server/trpc/example.test.ts
+++ server/trpc/example.test.ts
@@ -8,3 +8,10 @@ test('example tRPC test', async () => {
   const count = await caller.query('hello')
   expect(count).toBeGreaterThanOrEqual(0)
 })
+
+test('user tRPC test', async () => {
+  const ctx = await createContext()
+  const caller = router.createCaller(ctx)
+  const user = await caller.query('user', {userId: 1})
+  expect(user.name).toEqual('Jane Smith')
+})
--- server/trpc/router.ts
+++ server/trpc/router.ts
@@ -1,8 +1,27 @@
-import { createRootRouter } from './createRouter'
+import { createRootRouter, createRouter } from './createRouter'
 import { prisma } from './prisma'
+import { z } from 'zod'
+import { td2Data } from '~/shared/d2-dummy-data'
 
-export const router = createRootRouter().query('hello', {
+export const helloRouter = createRouter().query('hello', {
   async resolve() {
     return await prisma.user.count()
   }
-})
+});
+
+const userRouter = createRouter().query('user', {
+  input: z.object({
+    userId: z.number().int().nonnegative()
+  }),
+  resolve({input})  {
+    return {
+      userId: input.id,
+      name: td2Data.users[input.userId].name
+    };
+  },
+});
+
+export const router = createRootRouter()
+  .merge(helloRouter)
+  .merge(userRouter)
+;
