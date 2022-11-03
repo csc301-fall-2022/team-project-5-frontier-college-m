88e8c59a195246534e0e1ca1afbf74cf3ba7a6f3
GregSherman <greg.sherman@mail.utoronto.ca>
2022-11-02 22:55:16 -0400

    Created the announcements endpoint as well as 3 tests, one for each input field.

--- server/trpc/example.test.ts
+++ server/trpc/example.test.ts
@@ -1,5 +1,5 @@
 import { createContext } from './createContext'
-import { router } from './router'
+import { router, announcementsRouter } from './router'
 
 test('example tRPC test', async () => {
   const ctx = await createContext()
@@ -7,4 +7,31 @@ test('example tRPC test', async () => {
 
   const count = await caller.query('hello')
   expect(count).toBeGreaterThanOrEqual(0)
+})
+
+test('announcements tRPC test count', async () => {
+  const ctx = {userID: 2, maxCount: 3}
+  const caller = announcementsRouter.createCaller(ctx)
+
+  const announcements = await caller.query('announcements')
+  expect(announcements).toHaveLength(3)
+})
+
+test('announcements tRPC test IDs', async () => {
+  const ctx = {userID: 2, maxCount: 5}
+  const caller = announcementsRouter.createCaller(ctx)
+
+  const announcements = await caller.query('announcements')
+  expect(announcements[0].id).toEqual(0)
+  expect(announcements[4].id).toEqual(5)
+})
+
+test('announcements tRPC test date', async () => {
+  const ctx = {userID: 2, maxCount: 5, noEarlierThan: new Date('2022-11-01T11:16:01')}
+  const caller = announcementsRouter.createCaller(ctx)
+
+  const announcements = await caller.query('announcements')
+  for (const announcement of announcements) {
+    expect(announcement.sentAt >= ctx.noEarlierThan).toBeTruthy()
+  }
 })
--- server/trpc/router.ts
+++ server/trpc/router.ts
@@ -1,8 +1,49 @@
 import { createRootRouter } from './createRouter'
 import { prisma } from './prisma'
+import { td2Data } from '~~/shared/d2-dummy-data'
 
 export const router = createRootRouter().query('hello', {
   async resolve() {
     return await prisma.user.count()
+  }
+})
+
+// the input is {userID: number, maxCount: number, noEarlierThan: Date}
+export const announcementsRouter = router.query('announcements', {
+  async resolve(req) {
+    const maxCount = req.ctx.maxCount
+    const userID = req.ctx.userID
+    const noEarlierThan = req.ctx.noEarlierThan
+
+    // Find the group IDs of the user
+    const userGroups: { [id: number]: string } = td2Data.userGroups[userID]
+    const groupIDs = Object.keys(userGroups).map((id) => parseInt(id))
+
+    const announcementsList = []
+    let count = 0
+
+    // Find the announcements for each groupID and add it to the list
+    // Only if the noEarlierThan and maxCount constraint is satisfied
+    for (const groupID of groupIDs) {
+      const announcements = td2Data.eventAnnouncements[groupID]
+
+      if (announcements) {
+        for (const announcement of announcements) {
+          announcement.sentAt = new Date(announcement.sentAt)
+          if (announcement.sentAt < noEarlierThan) {
+            continue
+          }
+          announcementsList.push(announcement)
+          count++
+          if (count >= maxCount) {
+            break
+          }
+        }
+      if (count >= maxCount) {
+        break
+      }
+      }
+    }
+    return await announcementsList
   }
 })
