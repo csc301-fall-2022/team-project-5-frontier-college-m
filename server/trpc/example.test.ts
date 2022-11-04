1915e77de475ea64827cedba1516b23fbf49ea48
Patrick Zhou <44577726+PatrickZhou17@users.noreply.github.com>
2022-11-04 13:14:15 -0400

    tests for eventDetails endpoint (#30)
    * test for event endpoint
    
    * fix eventId
    
    Co-authored-by: Samm Du <hmu@sammdu.com>

--- server/trpc/example.test.ts
+++ server/trpc/example.test.ts
@@ -1,2 +1,22 @@
 import { createContext } from './createContext'
 import { router } from './router'
+
+test('eventDetails tRPC test name', async () => {
+  const ctx = await createContext()
+  const caller = router.createCaller(ctx)
+
+  const answer = await caller.query('eventDetails', {
+    eventId: 0
+  })
+  expect(answer.name).toBe('Figma Tutoring')
+})
+
+test('eventDetails tRPC test type', async () => {
+  const ctx = await createContext()
+  const caller = router.createCaller(ctx)
+
+  const answer = await caller.query('eventDetails', {
+    eventId: 1
+  })
+  expect(answer.type).toBe('recurring')
+})
