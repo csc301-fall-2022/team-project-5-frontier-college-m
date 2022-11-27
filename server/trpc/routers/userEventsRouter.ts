2f65a1b3daa8a9026ef9f0ccef5242c973f78b2b
Patrick Zhou <44577726+PatrickZhou17@users.noreply.github.com>
2022-11-26 22:57:18 -0500

    update  userEvents

--- server/trpc/routers/userEventsRouter.ts
+++ server/trpc/routers/userEventsRouter.ts
@@ -1,32 +1,44 @@
 
 import { z } from 'zod'
 import { auth, api } from '../salesforce'
-import fetch from 'node-fetch'
 import { createRouter } from '../createRouter'
 import { td2Data } from '~/shared/d2-dummy-data'
 
 /**
- * Return a list of programs from a given user.
+ * Return a list of announcements that pertain to a given user.
  */
-const userEventsRouter = createRouter().query('userEvents', {
+ export const userEventsRouter = createRouter().query('userEvents', {
   input: z.object({
     userID: z.string()
   }),
 
 // SELECT Id, Program_Name__c, RecordTypeId FROM ProgPar__C
 
-async resolve({input}) {
-  const response = await fetch( `${process.env.SALESFORCE_URL}/?q=SELECT ID, Program_Name__c, RecordTypeId FROM ProgPar__C WHERE ID='${input.userID}'` , {
-    method: 'GET',
-    headers: {
-      'Authorization': `Bearer ${process.env.ACCESS_TOKEN}`
+  async resolve({input}) {
+    if (!auth.token) {
+      await auth.getBearerToken(api)
     }
-  })
-  const data = await response.json()
-  const userEvents = data.record[0] // not sure
-  return {
-    userID: userEvents.ID,
-    events: userEvents.Program_Name__c // not sure if it forms array 
+    const qString = `SELECT ID, Participant_Contact__c, Program_Name__c, Program_OFFERING__c, RecordTypeId FROM ProgPar__C WHERE Patricipant_Contact__c='${input.userID}'`
+    let data = await api.query(qString, auth.token as string)
+
+    if (data && data.errorCode && data.errorCode === 'INVALID_SESSION_ID') {
+      await auth.getBearerToken(api)
+      data = await api.query(qString, auth.token as string)
+    }
+
+    const userEvents: Set<string> = new Set()
+    for (let i = 0; i < data.record[0].length; i++) {
+      if(data.record[0][i].Patricipant_Contact__c === input.userID){
+        userEvents.add(data.record[0][i].Program_OFFERING__c)
+      }
+
   }
-}
+
+    if (userEvents) {
+      return {
+        username: input.userID,
+        name: userEvents
+      }
+    }
+  }
 })
