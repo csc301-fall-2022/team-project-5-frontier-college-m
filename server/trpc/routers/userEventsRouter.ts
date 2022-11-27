53403a8e3b80cdd8355b201a65f17ae89d607a40
Samm Du <hmu@sammdu.com>
2022-11-27 15:12:29 -0500

    fixed data parsing mistakes

--- server/trpc/routers/userEventsRouter.ts
+++ server/trpc/routers/userEventsRouter.ts
@@ -11,32 +11,36 @@ export const userEventsRouter = createRouter().query('userEvents', {
     userId: z.string()
   }),
 
-  // SELECT Id, Program_Name__c, RecordTypeId FROM ProgPar__C
-
   async resolve({ input }) {
     if (!auth.token) {
       await auth.getBearerToken(api)
     }
-    const qString = `SELECT Id, Participant_Contact__c, Program_Name__c, Program_OFFERING__c, RecordTypeId FROM ProgPar__C WHERE Patricipant_Contact__c='${input.userId}'`
-    let data = await api.query(qString, auth.token as string)
 
+    const qString = `SELECT Id, Participant_Contact__c, Program_OFFERING__c FROM ProgPar__C WHERE Participant_Contact__c='${input.userId}'`
+
+    let data = await api.query(qString, auth.token as string)
     if (data && data.errorCode && data.errorCode === 'INVALID_SESSION_ID') {
       await auth.getBearerToken(api)
       data = await api.query(qString, auth.token as string)
     }
 
-    const userEvents: Set<string> = new Set()
-    for (let i = 0; i < data.record[0].length; i++) {
-      if (data.record[0][i].Patricipant_Contact__c === input.userId) {
-        userEvents.add(data.record[0][i].Program_OFFERING__c)
+    // only parse data if records are available
+    if (data.totalSize && data.totalSize > 0) {
+      const userEvents: Set<string> = new Set()
+      for (let i = 0; i < data.records.length; i++) {
+        userEvents.add(data.records[i].Program_Offering__c)
+      }
+
+      return {
+        userId: data.records[0].Participant_Contact__c,
+        events: [...userEvents] // convert set to array/list
       }
     }
 
-    if (data.record[0]) {
-      return {
-        userId: input.userId,
-        events: userEvents
-      }
+    // otherwise return events as empty
+    return {
+      userId: input.userId,
+      events: []
     }
   }
 })
