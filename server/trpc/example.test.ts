9857d14bf50b9277b3a8cec37f857548d0e95761
Patrick Zhou <44577726+PatrickZhou17@users.noreply.github.com>
2022-11-27 22:05:02 -0500

    rewrite unit tests for userEvents

--- server/trpc/example.test.ts
+++ server/trpc/example.test.ts
@@ -1,77 +1,72 @@
 import { createContext } from './createContext'
 import { router } from './router'
-import { td2Data } from '~/shared/d2-dummy-data'
 
-/**
- * Tests for /announcements
- */
+// /**
+//  * Tests for /announcements
+//  */
 
-test('announcements tRPC test count', async () => {
-  const ctx = await createContext()
-  const caller = router.createCaller(ctx)
+// test('announcements tRPC test count', async () => {
+//   const ctx = await createContext()
+//   const caller = router.createCaller(ctx)
 
-  const announcements = await caller.query('announcements', {
-    userId: 2,
-    maxCount: 3
-  })
-  expect(announcements).toHaveLength(3)
-})
+//   const announcements = await caller.query('announcements', {
+//     userId: 2,
+//     maxCount: 3
+//   })
+//   expect(announcements).toHaveLength(3)
+// })
 
-test('announcements tRPC test IDs', async () => {
-  const ctx = await createContext()
-  const caller = router.createCaller(ctx)
+// test('announcements tRPC test IDs', async () => {
+//   const ctx = await createContext()
+//   const caller = router.createCaller(ctx)
 
-  const announcements = await caller.query('announcements', {
-    userId: 2,
-    maxCount: 5
-  })
-  expect(announcements[0].id).toEqual(4)
-  expect(announcements[4].id).toEqual(6)
-})
+//   const announcements = await caller.query('announcements', {
+//     userId: 2,
+//     maxCount: 5
+//   })
+//   expect(announcements[0].id).toEqual(4)
+//   expect(announcements[4].id).toEqual(6)
+// })
 
-test('announcements tRPC test date', async () => {
-  const ctx = await createContext()
-  const caller = router.createCaller(ctx)
+// test('announcements tRPC test date', async () => {
+//   const ctx = await createContext()
+//   const caller = router.createCaller(ctx)
 
-  const dateLimit = new Date('2022-11-01T11:16:01')
-  const announcements = await caller.query('announcements', {
-    userId: 2,
-    maxCount: 5,
-    noEarlierThan: dateLimit
-  })
+//   const dateLimit = new Date('2022-11-01T11:16:01')
+//   const announcements = await caller.query('announcements', {
+//     userId: 2,
+//     maxCount: 5,
+//     noEarlierThan: dateLimit
+//   })
 
-  for (const announcement of announcements) {
-    expect(announcement.sentAt >= dateLimit).toBeTruthy()
-  }
-})
+//   for (const announcement of announcements) {
+//     expect(announcement.sentAt >= dateLimit).toBeTruthy()
+//   }
+// })
 
-/**
- * Tests for /eventDetails
- */
+// /**
+//  * Tests for /eventDetails
+//  */
 
-test('eventDetails tRPC test name', async () => {
-  const ctx = await createContext()
-  const caller = router.createCaller(ctx)
+// test('eventDetails tRPC test name', async () => {
+//   const ctx = await createContext()
+//   const caller = router.createCaller(ctx)
 
-  const answer = await caller.query('eventDetails', {
-    eventId: 0
-  })
-  expect(answer.name).toBe('Figma Tutoring')
-})
+//   const answer = await caller.query('eventDetails', {
+//     eventId: 0
+//   })
+//   expect(answer.name).toBe('Figma Tutoring')
+// })
 
-test('eventDetails tRPC test type', async () => {
-  const ctx = await createContext()
-  const caller = router.createCaller(ctx)
+// test('eventDetails tRPC test type', async () => {
+//   const ctx = await createContext()
+//   const caller = router.createCaller(ctx)
 
-  const answer = await caller.query('eventDetails', {
-    eventId: 1
-  })
-  expect(answer.type).toBe('recurring')
-})
-
-/**
- * Tests for /user
- */
+//   const answer = await caller.query('eventDetails', {
+//     eventId: 1
+//   })
+//   expect(answer.type).toBe('recurring')
+// })
 
 test('user tRPC test name', async () => {
   const ctx = await createContext()
@@ -98,31 +93,23 @@ test('user tRPC test userType', async () => {
 })
 
 /**
- * Tests for /user
- */
-
- test('user tRPC test', async () => {
-  const ctx = await createContext()
-  const caller = router.createCaller(ctx)
-  const user = await caller.query('user', { userId: '0056g000005JD9wAAG' })
-  expect(user.name).toEqual('Frontier College Hub')
-})
-
-/**
  * Tests for /userEvents
  */
 
- test('userEvents tRPC test userId', async () => {
+test('userEvents tRPC test userId', async () => {
   const ctx = await createContext()
   const caller = router.createCaller(ctx)
-  const userEvents = await caller.query('userEvents', { userId: '003Au000005D9H7IAK' })
+  const userEvents = await caller.query('userEvents', {
+    userId: '003Au000005D9H7IAK'
+  })
   expect(userEvents.userId).toEqual('003Au000005D9H7IAK')
 })
-
 
 test('userEvents tRPC test event', async () => {
   const ctx = await createContext()
   const caller = router.createCaller(ctx)
-  const userEvents = await caller.query('userEvents', { userId: '003Au000005D9H7IAK' })
+  const userEvents = await caller.query('userEvents', {
+    userId: '003Au000005D9H7IAK'
+  })
   expect(userEvents.events[0]).toEqual('a26Au00000008tdIAA')
 })
