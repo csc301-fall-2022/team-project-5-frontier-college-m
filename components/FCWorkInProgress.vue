cb569b0c57790b03d1eb109454a542d933d36a1b
Michael Lai <60846745+michaelkplai@users.noreply.github.com>
2022-11-30 16:40:35 -0500

    frontend polish (#70)
    Co-authored-by: michaelkplai <michael.kplai@gmail.com>

---
+++ components/FCWorkInProgress.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Work in Progress</h1>
+</template>
+
+<style scoped lang="scss">
+h1 {
+  color: white;
+  padding: 40px;
+  text-align: center;
+}
+</style>
---
+++ pages/chat.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+definePageMeta({
+  title: 'Chat',
+  showBack: true
+})
+</script>
+
+<template>
+  <FCWorkInProgress />
+</template>
