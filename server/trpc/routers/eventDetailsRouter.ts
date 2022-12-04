4df280f34a573b39f8a5a3a6e5bcc00488d8553b
Patrick Zhou <44577726+PatrickZhou17@users.noreply.github.com>
2022-12-04 15:17:50 -0500

    add fileSharing field in the eventDetailsRouter (#79)
    * add fileSharing field
    
    * removed console log
    
    Co-authored-by: Samm Du <hmu@sammdu.com>

--- server/trpc/routers/eventDetailsRouter.ts
+++ server/trpc/routers/eventDetailsRouter.ts
@@ -12,12 +12,11 @@ export const eventDetailsRouter = createRouter().query('eventDetails', {
   }),
 
   async resolve({ input }) {
-    const qString = `SELECT Id, Name, Program_Description__c, Goals__c, Type__c, Contact_Person__c, OwnerId, Regional_Record_owner_Contact__c, Reporting_Region__c, Start_Date__c, End_Date__c, Delivery_Method__c, Program_Offering_Schedule__c, Location_Label__c, Location_Address__c, RecordTypeId FROM Program__c WHERE Id ='${input.eventId}'`
+    const qString = `SELECT Id, Name, Program_Description__c, Goals__c, Type__c, Contact_Person__c, OwnerId, Regional_Record_owner_Contact__c, Reporting_Region__c, Start_Date__c, End_Date__c, Delivery_Method__c, Program_Offering_Schedule__c, Location_Label__c, Location_Address__c, RecordTypeId, File_Sharing__c FROM Program__c WHERE Id ='${input.eventId}'`
     const resp = await api.query(qString, auth)
     const data = await resp.json()
 
     const program = data.records[0]
-
     const recordTypeQuery = `SELECT Id, Name, DeveloperName FROM RecordType WHERE Id = '${program.RecordTypeId}'`
     const recordResp = await api.query(recordTypeQuery, auth)
     const recordData = await recordResp.json()
@@ -41,7 +40,8 @@ export const eventDetailsRouter = createRouter().query('eventDetails', {
         ),
         locationLabel: program.Location_Label__c,
         locationAddress: program.Location_Address__c,
-        recordType: recordData.records[0].Name
+        recordType: recordData.records[0].Name,
+        fileSharing: program.File_Sharing__c
       }
     }
   }
